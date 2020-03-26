3d4d7cd4c2b202b0484b644699f42a24caaa963c
IkaroFeitosa <ikaromsn@hotmail.com>
2020-03-26 10:03:31 -0300

    Updates

---
+++ precache-manifest.577be550b959e31e3bc620a81c8ac4e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "726795c705809d7338a76f73f4d2223c",
+    "url": "/Login_facebook/index.html"
+  },
+  {
+    "revision": "45335cbb9c6c3e7cfcbb",
+    "url": "/Login_facebook/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f13c60b92cd9260ca3b6",
+    "url": "/Login_facebook/static/js/2.0653dae1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Login_facebook/static/js/2.0653dae1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45335cbb9c6c3e7cfcbb",
+    "url": "/Login_facebook/static/js/main.d1147bad.chunk.js"
+  },
+  {
+    "revision": "a1c9ba9f7412272b1304",
+    "url": "/Login_facebook/static/js/runtime-main.56d73a33.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Login_facebook/static/media/logo.5d5d9eef.svg"
+  }
+]);
